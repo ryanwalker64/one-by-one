965e78a2e35dd564f2515f34704c0141729bafba
ryanwalker64 <ryan.walkerz46@gmail.com>
2023-05-15 08:18:09 +1200

    height fix

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -41,83 +41,86 @@ function App() {
   // bg-[color:#FFF5EA]
 
   return (
-    <div className="min-h-screen bg-black text-white px-8 pt-8 pb-14 flex flex-col justify-center">
+    <div class="absolute inset-0">
 
-     
+      <div className="min-h-screen bg-black text-white px-8 pt-8 pb-14 flex flex-col justify-center">
 
-      <div className="grow flex flex-col justify-center">
-      <ol className="list-decimal list-outside pl-9">
-      {screen == 4 && priorities.map(p => {
-        return (
-          <motion.li 
-          initial={{ opacity: 0, }}
-          animate={{ opacity: 0.5, }}
-          transition={{ duration: 0.5 }}
-          className="opacity-50 font-[100] text-4xl mb-6" key={p.order}>
-            {/* <p className="text-sm font-thin">priority {p.order}</p> */}
-            {p.text}
-            {/* <p className="text-3xl font-bold">{p.text}</p> */}
-          </motion.li>
-        )
-      })}
-      </ol>
-        {/* {screen > 0 &&  <span onClick={() => setScreen(prev => prev - 1)} className="text-sm font-thin">go back</span>} */}
-        {screen === 0 && 
-          <LayoutScreen 
-            setScreen={() => setScreen(1)}
-            heading="three tasks, one by one.">
-            <p onClick={() => setScreen(prev => prev + 1)} className="flex gap-1 font-thin text-xl items-center">let's start
-              <span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                  </svg>
-              </span>
-            </p>
-          </LayoutScreen>}
+      
 
-          {screen === 1 && 
-          <LayoutScreen setScreen={setScreen} heading="what needs to be done?">
-            <NewTaskInput 
-            placeholder="your first task" 
-            addPriority={addPriority}
-            setScreen={() => setScreen(prev => prev + 1)}/>
-          </LayoutScreen>}
+        <div className="grow flex flex-col justify-center">
+        <ol className="list-decimal list-outside pl-9">
+        {screen == 4 && priorities.map(p => {
+          return (
+            <motion.li 
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 0.5, }}
+            transition={{ duration: 0.5 }}
+            className="opacity-50 font-[100] text-4xl mb-6" key={p.order}>
+              {/* <p className="text-sm font-thin">priority {p.order}</p> */}
+              {p.text}
+              {/* <p className="text-3xl font-bold">{p.text}</p> */}
+            </motion.li>
+          )
+        })}
+        </ol>
+          {/* {screen > 0 &&  <span onClick={() => setScreen(prev => prev - 1)} className="text-sm font-thin">go back</span>} */}
+          {screen === 0 && 
+            <LayoutScreen 
+              setScreen={() => setScreen(1)}
+              heading="three tasks, one by one.">
+              <p onClick={() => setScreen(prev => prev + 1)} className="flex gap-1 font-thin text-xl items-center">let's start
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                    </svg>
+                </span>
+              </p>
+            </LayoutScreen>}
 
-          {screen === 2 && 
-          <LayoutScreen setScreen={setScreen} heading="what are you putting off?">
-            <NewTaskInput 
-            placeholder="your second task" 
-            addPriority={addPriority}
-            setScreen={() => setScreen(prev => prev + 1)}/>
-          </LayoutScreen>}
+            {screen === 1 && 
+            <LayoutScreen setScreen={setScreen} heading="what needs to be done?">
+              <NewTaskInput 
+              placeholder="your first task" 
+              addPriority={addPriority}
+              setScreen={() => setScreen(prev => prev + 1)}/>
+            </LayoutScreen>}
 
-          {screen === 3 && 
-          <LayoutScreen setScreen={setScreen} heading="what would be nice to do?">
-            <NewTaskInput 
-            placeholder="your second task" 
-            addPriority={addPriority}
-            setScreen={() => setScreen(prev => prev + 1)}/>
-          </LayoutScreen>}
+            {screen === 2 && 
+            <LayoutScreen setScreen={setScreen} heading="what are you putting off?">
+              <NewTaskInput 
+              placeholder="your second task" 
+              addPriority={addPriority}
+              setScreen={() => setScreen(prev => prev + 1)}/>
+            </LayoutScreen>}
 
-          {screen === 4 && 
-          <LayoutScreen setScreen={setScreen} heading="are you ready?">
-            <p onClick={() => setScreen('focus')} className="flex gap-1 font-thin text-xl">swipe to start
-              <span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                  </svg>
-              </span>
-            </p>
-          </LayoutScreen>}
+            {screen === 3 && 
+            <LayoutScreen setScreen={setScreen} heading="what would be nice to do?">
+              <NewTaskInput 
+              placeholder="your second task" 
+              addPriority={addPriority}
+              setScreen={() => setScreen(prev => prev + 1)}/>
+            </LayoutScreen>}
 
-          {screen === 'focus' && 
-          <FocusScreen 
-          key={priorities[activeTask].order} 
-          setScreen={setScreen} 
-          task={priorities[activeTask]}/>}
+            {screen === 4 && 
+            <LayoutScreen setScreen={setScreen} heading="are you ready?">
+              <p onClick={() => setScreen('focus')} className="flex gap-1 font-thin text-xl">swipe to start
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                    </svg>
+                </span>
+              </p>
+            </LayoutScreen>}
+
+            {screen === 'focus' && 
+            <FocusScreen 
+            key={priorities[activeTask].order} 
+            setScreen={setScreen} 
+            task={priorities[activeTask]}/>}
+        </div>
+      
+
       </div>
-    
-
     </div>
   )
 }
